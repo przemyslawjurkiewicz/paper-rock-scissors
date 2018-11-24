a0fce27b665cd32382679accc9f545d5d69e12d8
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2018-11-24 18:54:51 +0100

    Add Game Body

--- js/script.js
+++ js/script.js
@@ -3,10 +3,15 @@
   var playerResult;
   var computerResult;
   var playerName;
-
+  var computerChoose;
+  var winner;
   var welcome = document.querySelector("header");
   var playerScoresBoard = document.getElementById("playerScores");
   var computerScoresBoard = document.getElementById("computerScores");
+  var chooseRockButton = document.getElementById("chooseRock");
+  var choosePaperkButton = document.getElementById("choosePaper");
+  var chooseScissorsButton = document.getElementById("chooseScissors");
+  var roundWinner = document.getElementById("result");
 
   document.addEventListener("DOMContentLoaded", function() {
     playerName = window.prompt("Add your Name:");
@@ -14,17 +19,64 @@
       "beforeend",
       "<p>Welcome " + playerName + " in the most popular game in the world.</p>"
     );
-    displayScores(0, 0);
+    playerResult = 0;
+    computerResult = 0;
+    displayScores(playerResult, computerResult);
   });
 
   var displayScores = function(playerResult, computerResult) {
-    playerScoresBoard.insertAdjacentHTML(
-      "afterbegin",
-      "<p>" + playerName + "</p><p>" + playerResult + "</p>"
-    );
-    computerScoresBoard.insertAdjacentHTML(
-        "afterbegin",
-        "<p>COMPUTER: </p><p>" + computerResult + "</p>"
-      );
+    playerScoresBoard.innerHTML =
+      "<p>" + playerName + " :</p><p>" + playerResult + "</p>";
+    computerScoresBoard.innerHTML =
+      "<p>COMPUTER: </p><p>" + computerResult + "</p>";
   };
+
+  var displayWinner = function (winner){
+    roundWinner.innerHTML = winner == "player" ? "Player win" : (winner == "computer" ? "Computer win": "Remis");
+  }
+
+  var playerMove = function(playerChoose) {
+    console.log("player" + playerChoose);
+    computerMove();
+    playerChoose == computerChoose
+      ? (winner="remis")
+      : (playerChoose == "rock" && computerChoose == "scissors") ||
+        (playerChoose == "paper" && computerChoose == "rock") ||
+        (playerChoose == "scissors" && computerChoose == "paper")
+      ? (playerResult += 1, winner="player")
+      : (computerResult += 1, winner="computer");
+    console.log(playerResult);
+    console.log(computerResult);
+    displayScores(playerResult, computerResult);
+    displayWinner(winner);
+  };
+
+  var computerMove = function() {
+    var x = Math.floor(Math.random() * 3 + 1);
+    console.log(x);
+    switch (x) {
+      case 1:
+        computerChoose = "rock";
+        break;
+      case 2:
+        computerChoose = "paper";
+        break;
+      case 3:
+        computerChoose = "scissors";
+        break;
+    }
+    console.log("comp:" + computerChoose);
+  };
+
+  chooseRockButton.addEventListener("click", function() {
+    playerMove("rock");
+  });
+
+  choosePaperkButton.addEventListener("click", function() {
+    playerMove("paper");
+  });
+
+  chooseScissorsButton.addEventListener("click", function() {
+    playerMove("scissors");
+  });
 })();
