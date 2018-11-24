c857e96e26e4d661db80913705d7e3bc0e8d776e
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2018-11-24 17:02:56 +0100

    Add Scoreboard

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
 "use strict";
 (function() {
+  var playerResult;
+  var computerResult;
+  var playerName;
+
   var welcome = document.querySelector("header");
+  var playerScoresBoard = document.getElementById("playerScores");
+  var computerScoresBoard = document.getElementById("computerScores");
 
   document.addEventListener("DOMContentLoaded", function() {
-    var playerName = window.prompt("Add your Name:");
+    playerName = window.prompt("Add your Name:");
     welcome.insertAdjacentHTML(
       "beforeend",
       "<p>Welcome " + playerName + " in the most popular game in the world.</p>"
     );
+    displayScores(0, 0);
   });
+
+  var displayScores = function(playerResult, computerResult) {
+    playerScoresBoard.insertAdjacentHTML(
+      "afterbegin",
+      "<p>" + playerName + "</p><p>" + playerResult + "</p>"
+    );
+    computerScoresBoard.insertAdjacentHTML(
+        "afterbegin",
+        "<p>COMPUTER: </p><p>" + computerResult + "</p>"
+      );
+  };
 })();
