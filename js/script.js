19ff217190963ec0fdac064c7919bd3ecee292a0
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-01-05 19:01:59 +0100

    Add loops for buttons and images

--- js/script.js
+++ js/script.js
@@ -1,47 +1,54 @@
 'use strict';
 (function() {
+  var params = {
+    playerResult: 0,
+    computerResult: 0,
+    playerName: 'player',
+    computerChoose: 'none',
+    winner: 'none',
+    roundsLimit: 4,
+    round: 1,
+  }
   //declarations of global variables
-  var playerResult;
-  var computerResult;
-  var playerName;
-  var computerChoose;
-  var winner;
-  var roundsLimit;
-  var round;
+  //var playerResult;
+ // var computerResult;
+ // var playerName;
+ // var computerChoose;
+  //var winner;
+  //var roundsLimit = 3;
+ // var round;
   var welcome = document.querySelector('header');
   var playerScoresBoard = document.getElementById('playerScores');
   var computerScoresBoard = document.getElementById('computerScores');
-  var chooseRockButton = document.getElementById('chooseRock');
-  var choosePaperkButton = document.getElementById('choosePaper');
-  var chooseScissorsButton = document.getElementById('chooseScissors');
   var roundWinner = document.getElementById('result');
   var newGameButton = document.getElementById('new-game');
   var hiddenElements = document.getElementsByClassName('hidden');
 
   //question about the name after load
   window.onload = function() {
-    playerName = window.prompt('Enter your Name:');
-    playerName = !playerName ? 'Player' : playerName;
+    // playerName = window.prompt('Enter your Name:');
+    //  playerName = !playerName ? 'Player' : playerName;
+    params.playerName = 'player';
     welcome.insertAdjacentHTML(
       'beforeend',
-      `<p>Welcome  ${playerName} in the most popular game in the world.</p>`
+      `<p>Welcome  ${params.playerName} in the most popular game in the world.</p>`
     );
   };
 
   //new game button
   newGameButton.addEventListener('click', function() {
     //question about round limit
-    enterRoundLimit();
+    //enterRoundLimit();
     //change visible elements hidden after the page has been loaded
-    for (var i = 0; i < hiddenElements.length; i++) {
-      hiddenElements[i].classList.add('visible');
-    }
+    //for (var i = 0; i < hiddenElements.length; i++) {
+    // hiddenElements[i].classList.add('visible');
+    //}
     //reset variables
-    round = 1;
-    playerResult = 0;
-    computerResult = 0;
+    params.round = 1;
+    params.playerResult = 0;
+    params.computerResult = 0;
     roundWinner.innerHTML = 'Here are the results.';
-    displayScores(playerResult, computerResult);
+    displayScores(params.playerResult, params.computerResult);
     document.getElementById('computerChooseImage').src = 'images/comp.png';
     document.getElementById('playerChooseImage').src = 'images/player.png';
     //hide new game button
@@ -53,60 +60,62 @@
   //question about round limit.
   var enterRoundLimit = function() {
     //if limit is wrong
-    roundsLimit = window.prompt('Enter the limit of rounds:');
-    isNaN(roundsLimit) || !roundsLimit
+    params.roundsLimit = window.prompt('Enter the limit of rounds:');
+    isNaN(params.roundsLimit) || !params.roundsLimit
       ? (alert('Enter the number !!!'), enterRoundLimit())
-      : roundsLimit;
+      : params.roundsLimit;
   };
 
   //score boards
   var displayScores = function(playerResult, computerResult) {
-    playerScoresBoard.innerHTML = `<p>${playerName}: </p><p> ${playerResult}</p>`;
+    playerScoresBoard.innerHTML = `<p>${params.playerName}: </p><p> ${playerResult}</p>`;
     computerScoresBoard.innerHTML = `<p>COMPUTER: </p><p> ${computerResult}</p>`;
   };
 
-    //player move after click 'choose butoons'
+  //player move after click 'choose butoons'
   var playerMove = function(playerChoose) {
+    playerImagesShow(playerChoose);
     computerMove();
-    if (playerChoose == computerChoose) {
-      winner = 'remis';
+    if (playerChoose == params.computerChoose) {
+      params.winner = 'remis';
     } else if (
-      (playerChoose == 'rock' && computerChoose == 'scissors') ||
-      (playerChoose == 'paper' && computerChoose == 'rock') ||
-      (playerChoose == 'scissors' && computerChoose == 'paper')
+      (playerChoose == 'rock' && params.omputerChoose == 'scissors') ||
+      (playerChoose == 'paper' && params.computerChoose == 'rock') ||
+      (playerChoose == 'scissors' && params.computerChoose == 'paper')
     ) {
-      playerResult += 1;
-      winner = 'player';
+      params.playerResult += 1;
+      params.winner = 'player';
     } else {
-      computerResult += 1;
-      winner = 'computer';
+      params.computerResult += 1;
+      params.winner = 'computer';
     }
-    displayScores(playerResult, computerResult);
-    displayWinner(winner);
+    displayScores(params.playerResult, params.computerResult);
+    displayWinner(params.winner);
     //check if this is the last round
-    if (round == roundsLimit) {
+    if (params.round == params.roundsLimit) {
       whoWinsAll();
     } else {
-      round += 1;
+      params.round += 1;
+      console.log(params.round);
     }
   };
 
   //display who win the round
   var displayWinner = function(win) {
     if (win == 'player') {
-      roundWinner.innerHTML = `Round ${round}/${roundsLimit}: win ${playerName}`;
+      roundWinner.innerHTML = `Round ${params.round}/${params.roundsLimit}: win ${params.playerName}`;
     } else if (win == 'computer') {
-      roundWinner.innerHTML = `Round ${round}/${roundsLimit}: win Computer`;
+      roundWinner.innerHTML = `Round ${params.round}/${params.roundsLimit}: win Computer`;
     } else {
-      roundWinner.innerHTML = `Round ${round}/${roundsLimit}: Remis`;
+      roundWinner.innerHTML = `Round ${params.round}/${params.roundsLimit}: Remis`;
     }
   };
 
   //who wins all rounds
   var whoWinsAll = function() {
-    if (playerResult == computerResult) {
+    if (params.playerResult == params.computerResult) {
       allRemis();
-    } else if (playerResult > computerResult) {
+    } else if (params.playerResult > params.computerResult) {
       allPlayer();
     } else {
       allComputer();
@@ -145,38 +154,67 @@
   //computer move after player move
   var computerMove = function() {
     var x = Math.floor(Math.random() * 3 + 1);
-    //display computer choise picture
+    //transform numbers to names of moves
     switch (x) {
       case 1:
-        computerChoose = 'rock';
-        document.getElementById('computerChooseImage').src = 'images/rocki.png';
+      params.computerChoose = 'rock';
+        compImagesShow(params.computerChoose);
         break;
       case 2:
-        computerChoose = 'paper';
-        document.getElementById('computerChooseImage').src =
-          'images/paperi.png';
+      params.computerChoose = 'paper';
+        compImagesShow(params.computerChoose);
         break;
       case 3:
-        computerChoose = 'scissors';
-        document.getElementById('computerChooseImage').src =
-          'images/scissorsi.png';
+      params.computerChoose = 'scissors';
+        compImagesShow(params.computerChoose);
         break;
     }
   };
 
-  //player chooise butoons, change pictures
-  chooseRockButton.addEventListener('click', function() {
-    playerMove('rock');
-    document.getElementById('playerChooseImage').src = 'images/rocki.png';
+  //Change pictures for comp move
+  function compImagesShow(compChooseed) {
+    document.querySelectorAll('.comp-image').forEach(function(e) {
+      //reset images
+      e.classList.remove('visible');
+      e.classList.add('hidden');
+    });
+    //visible image to choosed move
+    var selector = '#' + compChooseed;
+    document
+      .querySelector('#computerChooseImage')
+      .querySelector(selector)
+      .classList.add('visible');
+  }
+
+  //Change pictures for player move
+  function playerImagesShow(playerChooseed) {
+    //reset images
+    document.querySelectorAll('.player-image').forEach(function(e) {
+      e.classList.remove('visible');
+      e.classList.add('hidden');
+    });
+    //visible image to choosed move
+    var selector = '#' + playerChooseed;
+    document
+      .querySelector('#playerChooseImage')
+      .querySelector(selector)
+      .classList.add('visible');
+  }
+
+  //player choose butoons
+  document.querySelectorAll('.player-move').forEach(function(e) {
+    e.addEventListener('click', function() {
+      playerMove(e.getAttribute('data-move'));
+    });
   });
 
-  choosePaperkButton.addEventListener('click', function() {
-    playerMove('paper');
-    document.getElementById('playerChooseImage').src = 'images/paperi.png';
-  });
-
-  chooseScissorsButton.addEventListener('click', function() {
-    playerMove('scissors');
-    document.getElementById('playerChooseImage').src = 'images/scissorsi.png';
-  });
+  //IT IS GOOD TOO
+  /* var chooseButtons = document.querySelectorAll('.player-move');
+  for (var i = 0; i < chooseButtons.length; i++) {
+    var choosed = chooseButtons[i].getAttribute('data-move');
+    chooseButtons[i].addEventListener('click', function() {
+      playerMove(choosed);
+    });
+  }
+*/
 })();
